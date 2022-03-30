2fad65bdab29d250b794a6e8b703dd87f487ab54
Saphal7702 <saphal7702@gmail.com>
2022-03-30 17:22:14 -0600

    Update proc and ports

--- app.js
+++ app.js
@@ -551,6 +551,11 @@ app.post("/search", function(req, res){
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server started in given port");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully...");
 });
